92334c100a0760bc4f229b1f13ddd72774a80326
Caoimhe Chaos <caoimhechaos@protonmail.com>
2021-04-03 22:32:00 +0200

    Add comments to improve script readability.

--- nfc_reader.py
+++ nfc_reader.py
@@ -6,6 +6,7 @@ import logging
 import musicpd
 import time
 
+# Set up logging.
 argparser = argparse.ArgumentParser()
 argparser.add_argument('-l', '--loglevel',
 	help='Level of logging that should be passed to stdout (e.g. DEBUG, INFO)')
@@ -26,20 +27,28 @@ cli = musicpd.MPDClient()
 seen_nones = 0
 playing = False
 
+# Main loop; wait for NFC events and react accordingly.
 while True:
 	(id, text) = reader.read_no_block()
 	if id is None:
+		# No NFC tag detected. If this happens on more than 3 reads, pause playback.
 		if seen_nones > 3 and playing:
-			logging.debug('Seen no NFC card %d times in a row, stopping playback', seen_nones)
+			logging.debug('Seen no NFC card %d times in a row, stopping playback',
+						  seen_nones)
 			cli.connect()
 			cli.pause()
 			cli.disconnect()
 			playing = False
 
+		# Count how many times we've seen no NFC tag.
 		seen_nones += 1
+
+		# For some reason, read_no_block() will report None on every other call, so
+		# we will only log nones > 1.
 		if seen_nones > 1:
 			logging.debug('Seen no NFC card %d times in a row', seen_nones)
 	elif id != last_seen_tag_id:
+		# New NFC tag found; load the playlist matching the text of the tag.
 		playlist = text.rstrip()
 		cli.connect()
 		try:
@@ -54,6 +63,8 @@ while True:
 		cli.disconnect()
 		last_seen_tag_id = id
 	elif id == last_seen_tag_id and not playing:
+		# Same tag found again, but we paused playback above; resume
+		# playback from the previous position.
 		cli.connect()
 		try:
 			cli.play()
